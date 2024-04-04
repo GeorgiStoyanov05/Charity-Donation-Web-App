26fe7870f30ac861cc67090ec94f90b6e0aed2a8
GeorgiStoyanov05 <g.stoyanov05@gmail.com>
2024-04-04 08:31:14 +0300

    NUnit Test Project Innit

---
+++ CharityProject/CharityTests/UnitTest1.cs
@@ -0,0 +1,50 @@
+namespace CharityTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestFixture]
+        public class CaseServiceTests
+        {
+            [Test]
+            public void Test1()
+            {
+                Assert.Pass();
+            }
+        }
+
+        [TestFixture]
+        public class CommentServiceTests
+        {
+            [Test]
+            public void Test1()
+            {
+                Assert.Pass();
+            }
+        }
+
+        [TestFixture]
+        public class DonationServiceTests
+        {
+            [Test]
+            public void Test1()
+            {
+                Assert.Pass();
+            }
+        }
+
+        [TestFixture]
+        public class UpdateServiceTests
+        {
+            [Test]
+            public void Test1()
+            {
+                Assert.Pass();
+            }
+        }
+    }
+}
